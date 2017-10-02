4df06be21f20b942da525b44e31a2ee043ac23ed
Juan Cardona <juancruz.23@hotmail.com>
2017-10-02 16:32:50 -0300

    03.02: discretization completed

--- 03/ex02/02.R
+++ 03/ex02/02.R
@@ -2,6 +2,8 @@
 univ <- read.csv("/home/juan/Documentos/unlu/bdm/tps/TP03/ds/encuesta_universitaria.csv") 
 # print data frame
 View(univ)
-tiempo_traslado <- univ[,5]
-univ[,5] <- cut(tiempo_traslado, breaks=5, dig.lab=7)
-univ[,5]
+tt <- univ[,5]
+tt.discretized.freq <- cut(tt, breaks=5, dig.lab=7)
+plot(tt.discretized.freq)
+tt.discretized.class <- cut(tt, breaks=c(0, 15, 30, 60, 120, max(tt, na.rm=TRUE)))
+plot(tt.discretized.class)
