036a0958e26adf4dc44cedd85b51ad314ec199fb
Juan Cardona <juancruz.23@hotmail.com>
2017-10-05 15:56:53 -0300

    03-04: compares distributions

--- 03/ex04/04.R
+++ 03/ex04/04.R
@@ -2,11 +2,16 @@
 univ <- read.csv("/home/juan/Documentos/unlu/bdm/tps/TP03/ds/encuesta_universitaria.csv") 
 # print data frame
 View(univ)
-tt <- univ[,5]
+tt.original <- univ[,5]
+tt.mean_replace <- univ[,5]
 # a) sustitution by mean
-tt.mean <- mean(tt, na.rm = TRUE)
-tt[is.na(tt)] <- tt.mean
+tt.mean <- mean(tt.mean_replace, na.rm = TRUE)
+tt.mean_replace[is.na(tt.mean_replace)] <- tt.mean
 # b) hot deck
 # install.packages("hot.deck")
 univ.no_nulls <- hot.deck(univ)
 tt.hotdeck <- as.data.frame(univ.no_nulls$data)[,5]
+# c) compare distributions
+hist(tt.original[tt.original<300], breaks=30, main = "Original", ylim=c(0,2000), xlab = "Tiempo de traslado")
+hist(tt.mean_replace[tt.mean_replace<300], breaks=30, main = "Mean", ylim=c(0,2000), xlab = "Tiempo de traslado")
+hist(tt.hotdeck[tt.mean_replace<300], breaks=30, main = "Hot-deck", ylim=c(0,2000), xlab = "Tiempo de traslado")
