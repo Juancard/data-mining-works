9f4fff7af7062f946e57408fa79c9d0c022db1a6
Juan Cardona <juancruz.23@hotmail.com>
2017-10-05 15:20:45 -0300

    03-03B: compares distributions

--- 03/ex03/03B.R
+++ 03/ex03/03B.R
@@ -5,9 +5,16 @@ View(univ)
 tt <- univ[,5]
 # remove null values
 tt.no_null <- tt[!is.na(tt)]
+# remove outliers
+tt.no_outliers = tt.no_null[tt.no_null<300]
+# distribution of original data
+hist(tt.no_outliers, main = "Original")
 # a) minmax normalization
-tt.minmax <- (tt.no_null-min(tt.no_null))/(max(tt.no_null)-min(tt.no_null))
+tt.minmax <- (tt.no_outliers-min(tt.no_outliers))/(max(tt.no_outliers)-min(tt.no_outliers))
+hist(tt.minmax, main = " Minmax")
 # b) z-score normalization
-tt.zscore <- (tt.no_null - mean(tt.no_null)) / sd(tt.no_null)
+tt.zscore <- (tt.no_outliers - mean(tt.no_outliers)) / sd(tt.no_outliers)
+hist(tt.zscore, main = "Z-score")
 # c) decimal scale normalization
-tt.decimal_scale <- tt.no_null / 10000
+tt.decimal_scale <- tt.no_outliers / 10000
+hist(tt.decimal_scale, main = "Decimal Scale")
