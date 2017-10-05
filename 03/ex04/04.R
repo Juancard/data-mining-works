6aef5da4b8674c67fdd585123d5fa6c528f846e7
Juan Cardona <juancruz.23@hotmail.com>
2017-10-05 16:01:32 -0300

    03-04: compares distributions

--- 03/ex04/04.R
+++ 03/ex04/04.R
@@ -12,6 +12,7 @@ tt.mean_replace[is.na(tt.mean_replace)] <- tt.mean
 univ.no_nulls <- hot.deck(univ)
 tt.hotdeck <- as.data.frame(univ.no_nulls$data)[,5]
 # c) compare distributions
-hist(tt.original[tt.original<300], breaks=30, main = "Original", ylim=c(0,2000), xlab = "Tiempo de traslado")
-hist(tt.mean_replace[tt.mean_replace<300], breaks=30, main = "Mean", ylim=c(0,2000), xlab = "Tiempo de traslado")
-hist(tt.hotdeck[tt.mean_replace<300], breaks=30, main = "Hot-deck", ylim=c(0,2000), xlab = "Tiempo de traslado")
+par(mfrow=c(1, 3))
+hist(tt.original[tt.original<300], breaks=30, main = "Original", ylim=c(0,1800), xlab = "Tiempo de traslado")
+hist(tt.mean_replace[tt.mean_replace<300], breaks=30, main = "Mean", ylim=c(0,1800), xlab = "Tiempo de traslado")
+hist(tt.hotdeck[tt.mean_replace<300], breaks=30, main = "Hot-deck", ylim=c(0,1800), xlab = "Tiempo de traslado")
